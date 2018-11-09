8555251d70ae46b1d8bb5450d933e347fbefd12b
lmm <752634866@qq.com>
2018-11-09 20:57:44 +0800

    TCP,UDP

--- 09_网络编程/03_TCP.py
+++ 09_网络编程/03_TCP.py
@@ -7,3 +7,9 @@ for data in [b'Michael', b'Tracy', b'Sarah']:
     print(s.recv(1024).decode('utf-8'))
 s.send(b'exit')
 s.close()
+
+# 小结
+# 用TCP协议进行Socket编程在Python中十分简单，
+# 对于客户端，要主动连接服务器的IP和指定端口，
+# 对于服务器，要首先监听指定端口，然后，对每一个新的连接，创建一个线程或进程来处理。
+# 通常，服务器程序会无限运行下去
