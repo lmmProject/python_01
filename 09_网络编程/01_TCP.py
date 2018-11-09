63480bb305b4eba813e01121875c5ab745cf287c
lmm <752634866@qq.com>
2018-11-09 17:31:33 +0800

    socket

---
+++ 09_网络编程/01_TCP.py
@@ -0,0 +1,38 @@
+# 创建一个基于TCP连接的Socket
+import socket
+
+# 创建Socket时，AF_INET指定使用IPv4协议，如果要用更先进的IPv6，就指定为AF_INET6。
+# SOCK_STREAM指定使用面向流的TCP协议，这样，一个Socket对象就创建成功，但是还没有建立连接。
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# 建立连接
+s.connect(('www.baidu.com', 80))
+
+# 发送请求
+# TCP连接创建的是双向通道，双方都可以同时给对方发数据。但是谁先发谁后发，怎么协调，要根据具体的协议来决定。
+# 例如，HTTP协议规定客户端必须先发请求给服务器，服务器收到后才发数据给客户端。
+s.send(b'GET / HTTP/1.1\r\nHost: www.baidu.com\r\nConnection: close\r\n\r\n')
+
+# 接受数据
+buffer = []
+while True:
+    # 每次最多接收1k字节
+    d = s.recv(1024)
+    if d:
+        buffer.append(d)
+    else:
+        break
+data = b''.join(buffer)
+# 接收数据时，调用recv(max)方法，一次最多接收指定的字节数，
+# 因此，在一个while循环中反复接收，直到recv()返回空数据，表示接收完毕，退出循环。
+
+# 关闭连接
+s.close()
+
+# 接收到的数据包括HTTP头和网页本身，
+# 我们只需要把HTTP头和网页分离一下，把HTTP头打印出来，网页内容保存到文件：
+header, html = data.split(b'\r\n\r\n', 1)
+print(header.decode('utf-8'))
+
+# 把接受到数据写到文件
+with open('baidu.html', 'wb') as f:
+    f.write(html)
